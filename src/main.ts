6b1291af88686d2a634a4d153ada573b502f576f
aydenl3 <soloyeti41@gmail.com>
2024-10-08 22:25:47 -0700

    Step 2 Complete: Added Clicking Button to Increase a Counter

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,25 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My amazing gameee";
+//Creates Game Title Text
+const gameName = "My amazing game";
 document.title = gameName;
-
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+//Creates Button
 const button = document.createElement("button");
 button.textContent = "🎷";
 app.append(button);
+
+//Creates Counter
+let num_notes:number = 0;
+button.addEventListener('click', (Increment: MouseEvent) => {
+    console.log(Increment); // This logs the event object
+    console.log('Notes Played!');
+    num_notes++;
+    counter.textContent = `Notes Played: ${num_notes}`;
+  });
+const counter = document.createElement("div");
+app.append(counter);
