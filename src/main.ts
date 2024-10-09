141546d53801bbd88200fe4a02f5104437dcd418
aydenl3 <soloyeti41@gmail.com>
2024-10-08 20:04:08 -0700

    Step 1 Complete: Added Button

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My amazing gamee";
+const gameName = "My amazing gameee";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+const button = document.createElement("button");
+button.textContent = "🎷";
+app.append(button);
