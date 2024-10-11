53ab2a6acfb49e3b724e4a77ee748b846070bc14
aydenl3 <soloyeti41@gmail.com>
2024-10-10 18:45:37 -0700

    Step 5 Complete: Added an Autoclick Upgrade

--- src/main.ts
+++ src/main.ts
@@ -13,30 +13,50 @@ app.append(header);
 const button = document.createElement("button");
 button.textContent = "🎷";
 app.append(button);
+button.addEventListener("click", () => incrementNotes(1));
 
-//Incrementer
+//Incrementers
 let num_notes: number = 0;
 function incrementNotes(amt: number): void {
   num_notes += amt;
   counter.textContent = `Notes Played: ${num_notes}`;
 }
 
+function incrementGrowthRate(amt: number, cost: number): void {
+  num_notes -= cost;
+  growth_rate += amt;
+}
+
 //Creates Counter
-button.addEventListener("click", () => incrementNotes(1));
 const counter = document.createElement("div");
 app.append(counter);
+
+//Creates Upgrade Button
+const upgrade_button1 = document.createElement("button");
+upgrade_button1.textContent = "🎸";
+app.append(upgrade_button1);
+upgrade_button1.addEventListener("click", () => incrementGrowthRate(1,10));
+upgrade_button1.disabled = true;
+
 
 //Auto-Clicker Logic
 let start_time = performance.now();
 let cache: number = 0;
+let growth_rate:number = 0;
 requestAnimationFrame(animate);
-function animate():void {
-  cache += (performance.now() - start_time);
-  if (cache >= 1000){
+function animate(): void {
+  cache += performance.now() - start_time;
+  if (cache >= 1000) {
     cache -= 1000;
-    incrementNotes(1);
+    incrementNotes(growth_rate);
   }
   start_time = performance.now();
+  if(num_notes >= 10){
+    upgrade_button1.disabled = false;
+  }
+  else{
+    upgrade_button1.disabled = true;
+  }
   requestAnimationFrame(animate);
   //console.log(cache);
 }
