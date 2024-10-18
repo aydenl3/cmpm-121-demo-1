80d3fc461ca5a5bce971fb3a440f4a6d2bb14645
aydenl3 <soloyeti41@gmail.com>
2024-10-17 20:55:08 -0700

    Step 10 Complete: Added two more Power-Ups

--- src/main.ts
+++ src/main.ts
@@ -35,17 +35,17 @@ function incrementGrowthRate(amt: number, cost: number): void {
 }
 
 interface Item {
-  name: string,
-  cost: number,
-  rate: number,
-  num: number,
-  emoji: string
-};
+  name: string;
+  cost: number;
+  rate: number;
+  num: number;
+  emoji: string;
+}
 
-const availableItems : Item[] = [
-  {name: "Bell", cost: 10, rate: 0.1, num: 0,emoji:"🔔"},
-  {name: "Piano", cost: 100, rate: 2, num: 0,emoji:"🎹"},
-  {name: "Guitar", cost: 1000, rate: 50, num: 0,emoji:"🎸"},
+const availableItems: Item[] = [
+  { name: "Bell", cost: 10, rate: 0.1, num: 0, emoji: "🔔" },
+  { name: "Piano", cost: 100, rate: 2, num: 0, emoji: "🎹" },
+  { name: "Guitar", cost: 1000, rate: 50, num: 0, emoji: "🎸" },
 ];
 
 //Creates Upgrade A
@@ -57,15 +57,13 @@ app.append(upgrade_buttonA);
 upgrade_buttonA.addEventListener("click", () => boughtUpgrade("Bell"));
 upgrade_buttonA.disabled = true;
 
-
-function boughtUpgrade(name:string) {
-  for (const item of availableItems){
-    if(name == item.name){
+function boughtUpgrade(name: string) {
+  for (const item of availableItems) {
+    if (name == item.name) {
       incrementGrowthRate(item.rate, item.cost);
       item.num++;
-      item.cost*=1.15;
+      item.cost *= 1.15;
     }
-
   }
 }
 
@@ -88,7 +86,6 @@ app.append(upgrade_buttonC);
 upgrade_buttonC.addEventListener("click", () => boughtUpgrade("Guitar"));
 upgrade_buttonC.disabled = true;
 
-
 //Button Cost Checker
 function updateDisplay(): void {
   counter.textContent = `Groove Garnered: ${Math.trunc(num_notes * 10) / 10}`;
@@ -101,22 +98,21 @@ function updateDisplay(): void {
   upgrade_buttonC.disabled = buttonEnableLogic("Guitar");
 }
 
-function updateUpgrades(name:string): string {
-  for(const item of availableItems){
-    if(name == item.name){
-      return(`Num ${item.emoji}'s: ${Math.floor(item.num)} | Cost of ${item.emoji}:${Math.trunc(item.cost * 10) / 10}`)
+function updateUpgrades(name: string): string {
+  for (const item of availableItems) {
+    if (name == item.name) {
+      return `Num ${item.emoji}'s: ${Math.floor(item.num)} | Cost of ${item.emoji}:${Math.trunc(item.cost * 10) / 10}`;
     }
   }
-  return("ERROR, no object found with that name");
+  return "ERROR, no object found with that name";
 }
-function buttonEnableLogic(name:string): boolean{
-  for(const item of availableItems){
-    if(name == item.name){
-      if(num_notes >= item.cost){
-        return (false);
-      }
-      else{
-        return (true);
+function buttonEnableLogic(name: string): boolean {
+  for (const item of availableItems) {
+    if (name == item.name) {
+      if (num_notes >= item.cost) {
+        return false;
+      } else {
+        return true;
       }
     }
   }
